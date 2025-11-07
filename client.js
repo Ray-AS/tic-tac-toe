107219110b32197e8840ccba1b82668862e468d3
Ray <abisuthans.business@gmail.com>
2025-11-07 10:04:06 -0500

    Implement game object

--- client.js
+++ client.js
@@ -21,17 +21,19 @@ const Gameboard = function () {
 
     const getBoard = function () {
         return board;
-    }
+    };
 
     const updateBoard = function (x, y, value) {
         if (board[y][x] == " ") {
             board[y][x] = value;
-            return `${value} placed at position (${x}, ${y})`;
+            console.log(`${value} placed at position (${x}, ${y})`);
+            return true;
         }
         else {
-            return "Position already occupied";
+            console.log("Position already occupied");
+            return false;
         }
-    }
+    };
 
     const checkState = function () {
         let winner = " ";
@@ -52,6 +54,7 @@ const Gameboard = function () {
         }
         else if (board.flat().every(cell => cell != " ")) {
             console.log("The game ended in a draw");
+            winner = "D";
         }
         else {
 
@@ -59,7 +62,7 @@ const Gameboard = function () {
         }
 
         return winner;
-    }
+    };
 
     return { getBoard, updateBoard, checkState };
 }();
@@ -67,11 +70,57 @@ const Gameboard = function () {
 const Player = function (marker) {
     const getMarker = function () {
         return marker;
-    }
+    };
 
     const makeMove = function (x, y, board) {
-        board.updateBoard(x, y, marker);
-    }
+        return board.updateBoard(x, y, marker);
+    };
 
     return { getMarker, makeMove };
 }
+
+const TicTacToe = function () {
+    const gameboard = Gameboard;
+    const p1 = Player("X");
+    const p2 = Player("O");
+    let turn = 0;
+
+    const playGame = function () {
+        while (gameboard.checkState() === " ") {
+            console.log(gameboard.getBoard());
+            if (turn % 2 == 0) {
+                console.log(`Player ${p1.getMarker()}'s turn.`);
+
+                let result = false;
+
+                while (!result) {
+                    let x = parseInt(prompt("Choose column: "));
+                    let y = parseInt(prompt("Choose row: "));
+
+                    result = p1.makeMove(x, y, gameboard);
+                }
+
+                turn += 1;
+            }
+            else {
+                console.log(`Player ${p2.getMarker()}'s turn.`);
+
+                let result = false;
+
+                while (!result) {
+                    let x = parseInt(prompt("Choose column: "));
+                    let y = parseInt(prompt("Choose row: "));
+
+                    result = p2.makeMove(x, y, gameboard);
+                }
+
+                turn += 1;
+            }
+        }
+    };
+
+    return { playGame }
+};
+
+const game = TicTacToe();
+game.playGame();
